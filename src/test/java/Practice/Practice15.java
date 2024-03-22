9129719308fb3a22528598b2a6da67e03183c62c
Angelina0504 <nataliya.yusupov@testpro.io>
2024-03-22 14:30:45 -0600

    PR 15
    PR15

--- src/test/java/Practice/Practice15.java
+++ src/test/java/Practice/Practice15.java
@@ -1,27 +1,28 @@
 package Practice;
 
 public class Practice15 {
-    public static int uniqueNumber(){
-        int x = 0;
-        while(x <= 10) {
-        System.out.print("x is =  "  + x);}
-        x++;
+    public static int task5_0() {
+        int x = 1;
+        while (x <= 10){
+            System.out.print(x);
+            x++;
+        }
         return x;
-
     }
-
-    public static int addNumber(){
+    public static int task5_1(){
         int i = 0;
         while (i < 10){
-            System.out.println("20:  ");
+            i++;
+            System.out.print("20  " );
         }
         return 20;
     }
     public static void main (String [] args){
-        int AddVariable = addNumber();
-        System.out.println("Return number is : " + AddVariable);
 
-        int NewNumbers = uniqueNumber();
-        System.out.println("Return numbers are: " + NewNumbers);
+        int NewNumbers = task5_0();
+        System.out.println("  Return numbers are: " + NewNumbers);
+
+        int AddVariable = task5_1();
+        System.out.println("Return number is: " + AddVariable);
     }
 }
