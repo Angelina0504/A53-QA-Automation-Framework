945d8139d120922659e17f4995fb856dc238a19d
Angelina0504 <nataliya.yusupov@testpro.io>
2024-04-22 10:22:26 -0600

    17 test overview
    multiple test cases for profile page

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -15,6 +15,7 @@ import java.util.UUID;
 public class BaseTest {
     public WebDriver driver = null;
     public String url = "https://qa.koel.app/";
+    public String urlProfile = "https://qa.koel.app/#!/profile";
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
@@ -35,6 +36,9 @@ public class BaseTest {
     }
     public void navigateToPage() {
         driver.get(url);
+    }
+    public void navigateToProfilePage(){
+        driver.get(urlProfile);
     }
     public void provideEmail(String email) {
         WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
@@ -74,5 +78,82 @@ public class BaseTest {
     public void isAvatarDisplayed() {
         WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
         Assert.assertTrue(avatarIcon.isDisplayed());
+    }
+
+    public void clickOnHomePage(){
+        WebElement homePage = driver.findElement(By.cssSelector("[class='home']"));
+        homePage.click();
+    }
+
+    public void isNewNameDisplayedCorrectly() {
+        WebElement newName = driver.findElement(By.cssSelector("span[class='name']"));
+}
+
+    public String generateRandomEmail() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public void provideNewEmail(String randomEmail) {
+        WebElement profileEmailField = driver.findElement(By.cssSelector("input[id='inputProfileEmail']"));
+        profileEmailField.clear();
+        profileEmailField.sendKeys(randomEmail);
+    }
+    public void provideProfileNewEmail(String emailField) {
+        WebElement profileEmailField = driver.findElement(By.cssSelector("input[id='inputProfileEmail']"));
+        profileEmailField.clear();
+        profileEmailField.sendKeys(emailField);
+    }
+
+    public String getProfileUpdatedSuccessMsg(){
+        WebElement notificationMsgEmailUpdated = driver.findElement(By.cssSelector("div.success.show"));
+        return notificationMsgEmailUpdated.getText();
+    }
+
+    public void clickOnShowNowPlayingCheckBox(){
+        WebElement checkBox = driver.findElement(By.cssSelector("input[name='notify']"));
+        checkBox.click();
+    }
+
+    public void clickOnAllSongs(){
+        WebElement allSongs = driver.findElement(By.cssSelector("[class='songs']"));
+        allSongs.click();
+    }
+
+    public void provideProfileNewPassword(String newPassword){
+        WebElement provideNewPassword = driver.findElement(By.cssSelector("input[name='new_password']"));
+        provideNewPassword.clear();
+        provideNewPassword.sendKeys(newPassword);
+    }
+
+    //public void doubleClickOnSong(){
+        //WebElement clickSong = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//tr[@class='song-item'][1]"));
+   // clickSong.click();
+    //}
+    public void clickPlayBtn(){
+        WebElement playNextBtn = driver.findElement(By.xpath("//i[@data-testid='play-next-btn']"));
+        WebElement playBtn = driver.findElement(By.cssSelector("span[class='play']"));
+        //xpath("//span[@data-testid='play-btn']"));   both works
+        playNextBtn.click();
+        playBtn.click();
+    }
+
+    public void clickOnLogoutBtn(){
+        WebElement confirmBox = driver.findElement(By.cssSelector("[data-testid='btn-logout']"));
+        confirmBox.click();
+    }
+
+    public void clickOnConfirmBeforeClosingApp(){
+        WebElement confirmBox = driver.findElement(By.cssSelector("input[name='confirm_closing']"));
+        confirmBox.click();
+    }
+
+    public void clickOnExtraTabAlbumButton(){
+        WebElement extraAlbum = driver.findElement(By.cssSelector("button[id='extraTabAlbum']"));
+        extraAlbum.click();
+    }
+
+    public void clickOnOverlayCheckBox(){
+        WebElement overlayBox = driver.findElement(By.cssSelector("input[name='show_album_art_overlay']"));
+        overlayBox.click();
     }
 }
--- src/test/java/ProfileTests.java
+++ src/test/java/ProfileTests.java
@@ -1,26 +1,195 @@
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 public class ProfileTests extends BaseTest {
     @Test
     public void changeProfileName() throws InterruptedException {
-
         navigateToPage();
         provideEmail("nataliya.yusupov@testpro.io");
         providePassword("Ashatan5934$");
         clickSubmit();
+        Thread.sleep(4000);
         clickOnAvatar();
         String randomName = generateRandomName();
-        provideCurrentPassword("nataliya.yusupov@testpro.io");
+        provideCurrentPassword("Ashatan5934$");
         provideProfileName(randomName);
         clickSaveButton();
 
         Thread.sleep(2000);
         WebElement actualProfileName = driver.findElement(By.cssSelector("a.view-profile>span"));
         Assert.assertEquals(actualProfileName.getText(), randomName);
-
+    }
+    @Test
+    public void updatedUserNameOnHomePage() throws InterruptedException {
+        navigateToPage();
+        provideEmail("nataliya.yusupov@testpro.io");
+        providePassword("Ashatan5934$");
+        clickSubmit();
+        Thread.sleep(4000);
+        clickOnAvatar();
+        String randomName = generateRandomName();
+        provideCurrentPassword("Ashatan5934$");
+        provideProfileName(randomName);
+        clickSaveButton();
+        clickOnHomePage();
+        isNewNameDisplayedCorrectly();
+        //Assertion
+        WebElement newName = driver.findElement(By.cssSelector("span[class='name']"));
+        Assert.assertTrue(newName.isDisplayed());
     }
 
-}
+    @Test
+    public void updatedEmail() throws InterruptedException {
+        navigateToPage();
+        provideEmail("nataliya.yusupov@testpro.io");
+        providePassword("Ashatan5934$");
+        clickSubmit();
+        Thread.sleep(4000);
+        clickOnAvatar();
+        String randomEmail = generateRandomEmail();
+        provideCurrentPassword("Ashatan5934$");
+        provideNewEmail(randomEmail);
+        clickSaveButton();
+        WebElement profileNewEmail = driver.findElement(By.cssSelector("input[id='inputProfileEmail']"));
+        Assert.assertTrue(profileNewEmail.isDisplayed());
+    }
+    @Test
+    public void updatedEmailField() throws InterruptedException {
+        String message = "Profile updated.";
+        navigateToPage();
+        provideEmail("nataliya.yusupov@testpro.io");
+        providePassword("Ashatan5934$");
+        clickSubmit();
+        Thread.sleep(4000);
+        clickOnAvatar();
+        provideCurrentPassword("Ashatan5934$");
+        provideProfileNewEmail("nataliya.yusupov@testpro.io1");
+        clickSaveButton();
+        Assert.assertEquals(getProfileUpdatedSuccessMsg(), message);
+    }
 
+    //To run we have to change the email add.to nataliya.yusupov@testpro.io, and update the email
+    //The updatedEmailField is correct test case for update an email for the Profile Page
+    @Test
+    public void updatedPasswordField() throws InterruptedException {
+        String message = "Profile updated.";
+        navigateToPage();
+        provideEmail("nataliya.yusupov@testpro.io");
+        providePassword("Ashatan5934$");
+        clickSubmit();
+        Thread.sleep(4000);
+        clickOnAvatar();
+
+        provideCurrentPassword("Ashatan5934$");
+        provideProfileNewPassword("Ashatan5934$$");
+        clickSaveButton();
+        Assert.assertEquals(getProfileUpdatedSuccessMsg(), message);
+    }
+
+    @Test
+    public void themeIsChangedOnAllSongsPage() throws InterruptedException {
+        String updatedThemeColor = "Oak";
+
+        navigateToPage();
+        provideEmail("nataliya.yusupov@testpro.io");
+        providePassword("Ashatan5934$");
+        clickSubmit();
+        Thread.sleep(4000);
+        clickOnAvatar();
+        Thread.sleep(4000);
+        clickOnTheme();
+        Thread.sleep(4000);
+        //clickOnHomePage();
+        clickOnAllSongs();
+        Thread.sleep(4000);
+
+        WebElement siderBarColored = driver.findElement(By.cssSelector("section[class='music']"));
+        //WebElement themeOak = driver.findElement(By.cssSelector("[data-theme='oak']"));
+        //Assert.assertEquals(themeOak.getText(), updatedThemeColor);
+        Assert.assertTrue(siderBarColored.isDisplayed());
+    }
+    @Test
+    public void songIsNotAppearsAsNotification()throws InterruptedException{
+    navigateToPage();
+    provideEmail("nataliya.yusupov@testpro.io");
+    providePassword("Ashatan5934$");
+    clickSubmit();
+        Thread.sleep(4000);
+    clickOnAvatar();
+        Thread.sleep(4000);
+    clickOnShowNowPlayingCheckBox();
+        Thread.sleep(4000);
+    clickOnAllSongs();
+        Thread.sleep(4000);
+   // doubleClickOnSong();
+    clickPlayBtn();
+    Assert.assertTrue(isSongPlaying());
+    }
+    @Test
+    public void noNotification()throws InterruptedException{
+        String songIsPlayingWithoutNotification = "1st Song is playing.";
+        navigateToPage();
+        provideEmail("nataliya.yusupov@testpro.io");
+        providePassword("Ashatan5934$");
+        clickSubmit();
+        Thread.sleep(4000);
+        clickOnAvatar();
+        Thread.sleep(4000);
+        clickOnShowNowPlayingCheckBox();
+        Thread.sleep(4000);
+        clickOnAllSongs();
+        Thread.sleep(4000);
+        // doubleClickOnSong();
+        clickPlayBtn();
+        Assert.assertNotNull(songIsPlayingWithoutNotification);
+    }
+    @Test
+    public void logoutKoelApp()throws InterruptedException{
+        boolean changesNotSavedMsgIsDisplayed = true;
+        navigateToPage();
+        provideEmail("nataliya.yusupov@testpro.io");
+        providePassword("Ashatan5934$");
+        clickSubmit();
+        Thread.sleep(4000);
+        clickOnAvatar();
+        Thread.sleep(4000);
+        clickOnConfirmBeforeClosingApp();
+        Thread.sleep(4000);
+        clickOnLogoutBtn();
+        Thread.sleep(4000);
+       // Assert.assertEquals(getConfirmNotificationMsg(), changesNotSavedMsg);
+        Assert.assertTrue(changesNotSavedMsgIsDisplayed, "Changes you made may not be saved.");
+    }
+    @Test
+    public void translucentOverlay()throws InterruptedException{
+        String albumCoverIsNotOverlay ="Album cover is not blurred overlay.";
+        navigateToPage();
+        provideEmail("nataliya.yusupov@testpro.io");
+        providePassword("Ashatan5934$");
+        clickSubmit();
+        Thread.sleep(4000);
+        clickOnAvatar();
+        Thread.sleep(4000);
+        clickOnOverlayCheckBox();
+        clickOnExtraTabAlbumButton();
+        Assert.assertNotNull(albumCoverIsNotOverlay, "Album cover is not blurred overlay." );
+    }
+
+    public boolean isSongPlaying(){
+        WebElement soundBar = driver.findElement(By.xpath("//div[@data-testid='sound-bar-play']"));
+        return soundBar.isDisplayed();
+    }
+
+    public void isColorChanging(String newColor){
+        WebElement themeOak = driver.findElement(By.cssSelector("class=['non-mac with-extra-panel']"));
+        themeOak.sendKeys(newColor);
+    }
+
+    public void clickOnTheme() {
+        WebElement themeOak = driver.findElement(By.cssSelector("div[class='theme'][data-testid='theme-card-oak']"));
+        themeOak.click();
+    }
+}
